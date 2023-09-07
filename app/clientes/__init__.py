13508d5e378c58e4fac0923627d709eb04cff9eb
Arley Beltran <arleybeltran17@gmail.com>
2023-09-07 12:14:57 -0500

    blueprint

--- app/clientes/__init__.py
+++
@@ -1,7 +0,0 @@
-from flask import Blueprint
-
-clientes=Blueprint('clientes', __name__, url_prefix= '/clientes', template_folder='templates')
-
-
-
-from . import routes
--- app/config.py
+++
@@ -1,4 +0,0 @@
-class Config:
-
-    SQLALCHEMY_DATABASE_URI = 'mysql://root:admin1234@localhost:3311/flask-shopy-2687365' 
-    SECRET_KEY = "HIHHFOULHF"
